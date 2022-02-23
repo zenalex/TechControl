c1ac76334d158b6d8c75db1edb4520d2434083f1
pro5 <pro5@SRV-FRZ-WEB>
2022-02-23 12:20:08 +0300

    ФормированиеСмены.ФормаЭлемента - заполнение

--- code/Метаданные/Мониторинг/ФормированиеСмены/ФормированиеСмены.cs
+++ code/Метаданные/Мониторинг/ФормированиеСмены/ФормированиеСмены.cs
@@ -23,6 +23,20 @@ namespace TechControl.Метаданные.Мониторинг
         #endregion //Свойства
 
         #region Методы
+        protected override bool Handling()
+        {
+            РегистрМониторингТехники рег = РегистрМониторингТехники.Новый();
+            рег.New();
+            рег.Объекты = this.Объект;
+            foreach (var i in this.Таблица.Rows)
+            {
+                рег.Сотрудники = i.Сотрудник;
+                рег.Техника = i.Техника;
+                рег.СтатусТехники = i.СтатусТехники;
+                рег.AddMovement();
+            }
+            return рег.Post();
+        }
         #endregion //Методы
     }
 
