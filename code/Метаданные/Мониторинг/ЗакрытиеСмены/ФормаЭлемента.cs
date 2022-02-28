0f1b3e515b03731bf0c54b197cc4d9f67b8c30ec
Dertko Daniil <64313607+dertkoD@users.noreply.github.com>
2022-02-28 11:35:53 +0300

    Добавил форму для документа закрытие смены

---
+++ code/Метаданные/Мониторинг/ЗакрытиеСмены/ФормаЭлемента.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using NsgSoft.DataObjects;
+using NsgSoft.Forms;
+
+
+
+
+
+namespace TechControl.Метаданные.Мониторинг
+{
+    
+    public partial class ЗакрытиеСменыФормаЭлемента
+
+    {
+        public ЗакрытиеСменыФормаЭлемента()
+        {
+            InitializeComponent();
+		}
+
+		#region #Comments_Data# NsgSoft.Forms.NsgMultipleObjectElementForm
+		
+		#endregion //#Comments_Data# NsgSoft.Forms.NsgMultipleObjectElementForm
+
+		#region #Comments_Constructors# NsgSoft.Forms.NsgMultipleObjectElementForm
+		
+		#endregion //#Comments_Constructors# NsgSoft.Forms.NsgMultipleObjectElementForm
+
+		#region #Comments_Methods# NsgSoft.Forms.NsgMultipleObjectElementForm
+		
+		#endregion //#Comments_Methods# NsgSoft.Forms.NsgMultipleObjectElementForm
+
+		#region #Comments_Properties# NsgSoft.Forms.NsgMultipleObjectElementForm
+		
+		#endregion //#Comments_Properties# NsgSoft.Forms.NsgMultipleObjectElementForm
+
+	}
+    
+
+
+}
