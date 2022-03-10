a4a748e8d0f8789a754d61f8312a324061b61dc1
Dertko Daniil <64313607+dertkoD@users.noreply.github.com>
2022-03-10 15:05:35 +0300

    добавил формы элементов

---
+++ code/Метаданные/Мониторинг/Должности/ФормаЭлемента.Designer.cs
@@ -0,0 +1,145 @@
+using NsgSoft.Forms;
+using NsgSoft.DataObjects;
+namespace TechControl.Метаданные.Мониторинг
+
+{
+    
+    public partial class ДолжностиФормаЭлемента:NsgSoft.Forms.NsgMultipleObjectElementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgToolStrip2)).BeginInit();
+            this.nsgToolStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgAll)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgHandling)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgSave)).BeginInit();
+            this.panelButtons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgButtonBasic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.printButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentMovementsButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnUsersActions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showInListButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Comment)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentStructure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgVisualMultipleObject)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nsgEdit
+            // 
+            this.nsgEdit.Size = new System.Drawing.Size(116, 26);
+            // 
+            // nsgClose
+            // 
+            this.nsgClose.Location = new System.Drawing.Point(662, 1);
+            this.nsgClose.Size = new System.Drawing.Size(78, 26);
+            // 
+            // nsgAll
+            // 
+            this.nsgAll.Location = new System.Drawing.Point(276, 1);
+            this.nsgAll.Size = new System.Drawing.Size(95, 26);
+            // 
+            // nsgHandling
+            // 
+            this.nsgHandling.Location = new System.Drawing.Point(371, 1);
+            this.nsgHandling.Size = new System.Drawing.Size(84, 26);
+            // 
+            // nsgSave
+            // 
+            this.nsgSave.Location = new System.Drawing.Point(455, 1);
+            this.nsgSave.Size = new System.Drawing.Size(91, 26);
+            // 
+            // nsgButtonBasic
+            // 
+            this.nsgButtonBasic.Location = new System.Drawing.Point(276, 1);
+            // 
+            // Comment
+            // 
+            this.Comment.BackColor = System.Drawing.SystemColors.Window;
+            this.Comment.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Comment.ConfigurationName = "";
+            this.Comment.DisableLeaveControlEvent = false;
+            this.Comment.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Comment.FullName = "";
+            this.Comment.FullTextSearch = false;
+            this.Comment.HorizontalAlignment = System.Drawing.StringAlignment.Near;
+            this.Comment.IsButton = false;
+            this.Comment.IsInitialized = true;
+            this.Comment.IsPassword = false;
+            this.Comment.Location = new System.Drawing.Point(0, 0);
+            this.Comment.Margin = new System.Windows.Forms.Padding(0);
+            this.Comment.Mask = "";
+            this.Comment.MetaDataName = "";
+            this.Comment.MinimumSize = new System.Drawing.Size(4, 20);
+            this.Comment.Name = "NsgInput";
+            this.Comment.ObjectImages = null;
+            this.Comment.ObjectIndex = null;
+            this.Comment.ObjectStrings = null;
+            this.Comment.Requsite = "Идентификатор";
+            this.Comment.SearchFieldName = "";
+            this.Comment.ShowMultipleObjectInComboBox = false;
+            this.Comment.ShowRowCount = 10;
+            this.Comment.Size = new System.Drawing.Size(741, 23);
+            this.Comment.SourceObject = this.nsgVisualMultipleObject;
+            this.Comment.TabIndex = 0;
+            this.Comment.VerticalAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // ДолжностиФормаЭлемента
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(741, 410);
+            this.Name = "ДолжностиФормаЭлемента";
+            ((System.ComponentModel.ISupportInitialize)(this.nsgEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgToolStrip2)).EndInit();
+            this.nsgToolStrip2.ResumeLayout(false);
+            this.nsgToolStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgAll)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgHandling)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgSave)).EndInit();
+            this.panelButtons.ResumeLayout(false);
+            this.panelButtons.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgButtonBasic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.printButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentMovementsButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnUsersActions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showInListButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Comment)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.documentStructure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsgVisualMultipleObject)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        
+    }
+}
