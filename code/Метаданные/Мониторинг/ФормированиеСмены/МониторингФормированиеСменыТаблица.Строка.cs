d92a034054e44df44d704715e8230514af4aa9ed
pro5 <pro5@SRV-FRZ-WEB>
2022-03-25 19:28:19 +0300

    МониторингФормированиеСменыТаблица.Строка.БылаЗаправкаВТечениеСмены()

--- code/Метаданные/Мониторинг/ФормированиеСмены/МониторингФормированиеСменыТаблица.Строка.cs
+++ code/Метаданные/Мониторинг/ФормированиеСмены/МониторингФормированиеСменыТаблица.Строка.cs
@@ -5,8 +5,7 @@ using NsgSoft.DataObjects;
 using NsgSoft.Database;
 using System.IO;
 using NsgSoft.Common;
-
-
+using TechControl.Метаданные.Мониторинг;
 
 namespace TechControl.Метаданные._SystemTables
 {
@@ -14,17 +13,31 @@ namespace TechControl.Метаданные._SystemTables
 {
     public partial class Строка
     {
-        #region Данные
-        #endregion //Данные
-
-        #region Конструкторы
-        #endregion //Конструкторы
-
-        #region Свойства
-        #endregion //Свойства
-
-        #region Методы
-        #endregion //Методы
-    }
+            public bool БылаЗаправкаВТечениеСмены()
+            {
+                РегистрЗаправок регистрЗаправок = РегистрЗаправок.Новый();
+                регистрЗаправок.New();
+                NsgCompare cmp = new NsgCompare()
+                    .Add(РегистрЗаправок.Names.Техника, this.Техника);
+                if (this.СтатусТехники == СтатусТехники.ВРаботе)
+                {
+                    var rests = регистрЗаправок.GetCirculate(this.Время, DateTime.Now, cmp);
+                    return rests.Count > 0;
+                }
+                else
+                {
+                    ФиксацияИстории фиксацияИстории = ФиксацияИстории.Новый();
+                    фиксацияИстории.New();
+                    фиксацияИстории.Техника = this.Техника;
+                    фиксацияИстории.GetRest(this.Время.AddSeconds(-1));
+                    if (фиксацияИстории.СтатусТехники == СтатусТехники.ВРаботе)
+                    {
+                        var rests = регистрЗаправок.GetCirculate(фиксацияИстории.Дата, this.Время, cmp);
+                        return rests.Count > 0;
+                    }
+                }
+                return false;
+            }
+        }
 }
 }
